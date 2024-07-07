682ff81385704c4a2a88e4618b138e993d1453d4
min429 <lsm990429@gmail.com>
2024-07-07 19:20:28 +0900

    Initial commit

---
+++ src/main/java/com/min429/tourism_platform_server/TourismPlatformServerApplication.java
@@ -0,0 +1,13 @@
+package com.min429.tourism_platform_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourismPlatformServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourismPlatformServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/min429/tourism_platform_server/TourismPlatformServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.min429.tourism_platform_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourismPlatformServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
